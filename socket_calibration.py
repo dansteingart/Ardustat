dcae056c3e19a85ae348af8010849701a65a4b14
Eddie Nolan <eddiejnolan@gmail.com>
2011-07-26 14:14:57 -0400

    Deprecated simpardlibnet functions

--- socket_calibration.py
+++
@@ -1,9 +0,0 @@
-from simpardlibnet import ardustat as ard
-from time import sleep,time
-
-a = ard()
-
-a.connect(7777)
-
-a.calibrate(9970,14)
-
--- socket_test_inject_ocv.py
+++
@@ -1,8 +0,0 @@
-from simpardlibnet import ardustat as ard
-from time import sleep
-a = ard()
-
-a.connect(7777)
-a.ocv()
-sleep(.1)
-print a.rawread()
