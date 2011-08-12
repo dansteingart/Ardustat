6a72f9c054f58cc7a580437b86b5e53bb9e5c333
Dan Steingart <dan.steingart@gmail.com>
2011-08-12 14:51:02 -0400

    connecting sanity check added

--- Python_Client/sanitycheck_with_connect.py
+++ Python_Client/sanitycheck_with_connect.py
@@ -2,11 +2,30 @@ from pylab import *
 import numpy
 import ardustat_library_simple as ard
 import time
+import subprocess
+import os
+import glob
+import sys
 
+##Guess a serial port
+port = ""
+if os.name == "posix":
+	#try os x
+	if len(glob.glob("/dev/tty.u*")) > 0:
+		port = glob.glob("/dev/tty.u*")[0]
+	elif len(glob.glob("/dev/ttyUSB*")) > 0:
+		port = glob.glob("/dev/ttyUSB*")[0]
+	else:
+		print "can't see any ardustats.  PEACE."
+		sys.exit()
+	print port
+	
 
 #start a serial forwarder
-p = subprocess.Popen("python tcp_serial_redirect.py /dev/tty.usbmodemfa131 57600".split())
-
+p = subprocess.Popen(("python tcp_serial_redirect.py "+port+" 57600").split())
+print "waiting"
+time.sleep(5)
+print "going"
 #connecto to ardustat and setup resistance table
 a = ard.ardustat()
 a.connect(7777)
@@ -18,6 +37,12 @@ times = []
 potential = []
 current = []
 time_start = time.time()
+
+def blink():
+	a.rawwrite(" ")
+	time.sleep(.1)
+	aaaaa = 3
+
 def appender(reading):
 	print reading['cell_ADC'],read['current']
 	potential.append(reading['cell_ADC'])
@@ -27,6 +52,7 @@ def appender(reading):
 
 #Step through values
 output = 0
+blink()
 print a.ocv()
 for i in range(0,10):
 	time.sleep(.1)
@@ -35,11 +61,14 @@ for i in range(0,10):
 
 while output < 2:
 	output = output + .1
+	blink()
 	print a.potentiostat(output)
 	for i in range(0,3):
 		time.sleep(.1)
 		read = a.parsedread()
 		appender(read)
+
+blink()
 print a.ocv()
 for i in range(0,10):
 	time.sleep(.1)
@@ -49,12 +78,14 @@ for i in range(0,10):
 output = 0
 while output < .001:
 	output = output + .0001
+	blink()
 	a.galvanostat(output)
 	for i in range(0,3):
 		time.sleep(.1)
 		read = a.parsedread()
 		appender(read)
 
+blink()
 print a.ocv()
 for i in range(0,10):
 	time.sleep(.1)
