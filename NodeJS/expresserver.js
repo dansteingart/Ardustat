e57be5ea58535faee0c65f38948037616063d06c
Dan Steingart <dan.steingart@gmail.com>
2012-01-13 07:22:52 -0500

    CV added, Logging added

--- NodeJS/expresserver.js
+++ NodeJS/expresserver.js
@@ -1,61 +1,101 @@
-
 //Set it up
-var http = require("http");
-var url = require("url");
-var fs = require('fs');
-var glob = require('glob');
-var qs = require('querystring');
-var express = require('express'),
+var http = require("http"); //HTTP Server
+var url = require("url"); // URL Handling
+var fs = require('fs'); // Filesystem Access (writing files)
+var glob = require('glob'); // Easy directory searches (from python)
+var express = require('express'), //App Framework (similar to web.py abstraction)
     app = express.createServer();
 	app.use(express.bodyParser());
 	app.use(app.router);
-
-io = require('socket.io').listen(app);
+io = require('socket.io').listen(app); //Socket Creations
 io.set('log level', 1)
-var serialport = require("serialport")
-var SerialPort = require("serialport").SerialPort
+var serialport = require("serialport") //Serial Port Creation
+var SerialPort = require("serialport").SerialPort 
 
 var serialPort = new SerialPort(glob.globSync("/dev/tty.u*")[0],{baudrate:57600,parser:serialport.parsers.readline("\n") });
 var datastream = ""
 
+//Logging Hack because I'm tired 
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+//exec("ls -la", puts);
+
 
 //Express Redirects
+//Static Redirects
 app.use("/flot", express.static(__dirname + '/flot'));
 app.use("/socket.io", express.static(__dirname + '/node_modules/socket.io/lib'));
 
 
+//On Home Page
 app.get('/', function(req, res){
 	indexer = fs.readFileSync('index.html').toString()
     res.send(indexer);
 });
 
-
+//Define Debug Page 
+//Use for debugging ardustat features
 app.get('/debug', function(req, res){
 	indexer = fs.readFileSync('debug.html').toString()
     res.send(indexer);
 });
 
+//CV Page
+app.get('/cv', function(req, res){
+	indexer = fs.readFileSync('cv.html').toString()
+    res.send(indexer);
+});
+
+
+//Accept data (all pages, throws to setStuff()
 app.post('/senddata', setStuff,function(req, res,next){
 	console.log(req.body)
 	res.send(req.body)
 	
 });
 
-
+//Presocket Connect, may reuse when flat files/mongodb is implements
 app.post('/getdata',function(req, res){
 	res.send(req.app.settings)
 });
 
+//Start listener on this port
+//TODO: link to aruino id: e.g. board 16 is port 8016
 app.listen(8888);
 console.log('Express server started on port %s', app.address().port);
-
 
 //Program Functions
 function setStuff(req,res)
 {
+	//loops to false
+	//If arducomm (e.g. direct signal to ardustat)
 	if (req.body.arducomm != undefined) serialPort.write(req.body.arducomm)
+	
+	if (req.body.logger != undefined)
+	{
+		
+		logger = req.body.logger
+		//console.log(logger)
+		if (logger == "started")
+		{
+			console.log("starting log")
+			datafile = req.body.datafilename +"_"+ new Date().getTime().toString()+".ardudata"
+			
+		}
+		else if (logger = "stopped")
+		{
+			console.log("stopping log")
+		}
+	}
+
+	//If abstracted command (potentiostat,cv, etc)
 	if (req.body.command != undefined)
 	{
+		calibrate = false
+		cv = false
+		cycling = false
+		
 		command = req.body.command;
 		value = req.body.value;
 		if (command == "calibrate")
@@ -64,6 +104,11 @@ function setStuff(req,res)
 			calibrator(req.body.value);
 		}
 		
+		if (command == "ocv")
+		{
+			console.log("setting ocv");
+			ocv()
+		}
 		
 		if (command == "potentiostat")
 		{
@@ -80,13 +125,99 @@ function setStuff(req,res)
 			console.log("setting galvanostat");
 			moveground(value)
 		}
+		if (command == "cv")
+		{
+			console.log("setting cv!");
+		
+			cv_start_go(value)
+		}
 		
 	}
 	res.send(req.body)
 	
 }
 
+//Global Variables for CV
+cv = false
+cycling = false
+cv_set = 0
+cv_dir = 1
+cv_rate = 1000
+cv_max = 1
+cv_min = -1
+cv_cycle = 1
+cv_cycle_limit = 1
+cv_step = 0
+cv_time  = new Date().getTime()	
+cv_arr = []
+last_comm = ""
+cv_comm = ""
 
+//Global Variables for Logging
+logger = "stopped"
+datafile = ""
+//Abstracted Electrochemical Functions
+
+//Initiate CV
+function cv_start_go(value)
+{
+		cv_arr = []
+		moveground(2.5)
+		//convert mV/s to a wait time
+		cv_rate =  (1/parseFloat(value['rate']))*1000*5
+		cv_max = parseFloat(value['v_max'])
+		cv_min = parseFloat(value['v_min'])
+		cv_start = parseFloat(value['v_start'])
+		cv_cycle_limit = parseFloat(value['cycles']*2)
+		cv_cycle = 0
+		cv = true
+		cv_time  = new Date().getTime()	
+		cv_step = cv_start
+		potentiostat(cv_step)
+		
+}
+
+//Step through CV
+function cv_stepper()
+{
+	
+	time = new Date().getTime()	
+	if (time - cv_time > cv_rate)
+	{
+		console.log("next step")
+		cv_time = time 
+		cv_step = cv_step + cv_dir*.005
+		if (cv_step > cv_max & cv_dir == 1)
+		{
+			cv_dir = -1
+			cv_cycle++
+		}
+		
+		else if (cv_step < cv_min & cv_dir == -1)
+		{
+			cv_dir = 1
+			cv_cycle++
+		}
+		if (cv_cycle > cv_cycle_limit) 
+		{
+			cv = false
+			ocv()
+		}
+		else
+		{
+			console.log(cv_step)
+			potentiostat(cv_step)
+		}
+	}
+}
+
+//Set to OCV
+function ocv()
+{
+	toArd("-",0000)
+}
+
+//Set Potentiostat
 function potentiostat(value)
 {
 	value_to_ardustat = value / volts_per_tick;
@@ -94,6 +225,7 @@ function potentiostat(value)
 	
 }
 
+//Allow negative values
 function moveground(value)
 {
 	value_to_ardustat = value / volts_per_tick;
@@ -103,6 +235,7 @@ function moveground(value)
 	
 }
 
+//Set Galvanostat
 function galvanostat(value)
 {
 	//First Match R
@@ -137,10 +270,13 @@ function galvanostat(value)
 
 
 
+//Global Functions for Data Parsing
 id = 20035;
 vpt = undefined; //volts per tick
 mode = 0;
 var res_table;
+
+//Break Arduino string into useful object
 function data_parse(data)
 {
 	parts = data.split(",")
@@ -157,6 +293,7 @@ function data_parse(data)
 	out['ref_adc'] = parseFloat(parts[9])
 	out['twopointfive_adc'] = parseFloat(parts[10])
 	out['id'] = parseInt(parts[11])
+	out['last_comm'] = last_comm
 	//making sense of it
 	volts_per_tick = 	2.5/out['twopointfive_adc']
 	if (vpt == undefined) vptt = volts_per_tick;
@@ -207,21 +344,6 @@ function data_parse(data)
 }
 
 
-serialPort.on("data", function (data) {
-	if (data.search("GO")>-1)
-	{
-		foo = data_parse(data);
-		d = new Date().getTime()	
-		foo['time'] = d	 	
-		io.sockets.emit('new_data',{'ardudata':foo} )
-		app.set('ardudata',foo)
-		if (calibrate)
-		{
-			calibration_array.push(foo)
-		}
-	}
-});
-
 
 //CALIBRATION PORTION
 //What happens
@@ -234,6 +356,7 @@ callimit = 2
 calibration_array = []
 rfixed = 10000
 
+
 function calibrator(value)
 {
 	rfixed = parseFloat(value)
@@ -245,10 +368,8 @@ function calibrator(value)
 	setTimeout(function(){calibrate = true},100)
 }
 
-setInterval(function(){
-	serialPort.write("s0000")
-	if (calibrate)
-	{   
+function calibrate_step()
+{
 		counter++;
 		if (counter > 255)
 		{
@@ -292,16 +413,60 @@ setInterval(function(){
 			}
 		} 
 		setTimeout(function(){toArd("r",counter)},50);
-	
+}
+
+
+//Serial Port Interactions
+
+setInterval(function(){
+	serialPort.write("s0000")
+	if (calibrate)
+	{   
+		calibrate_step()
 	}
+	
+	if(cv)
+	{
+		cv_stepper()
+	}
+
 },100)
 
-//Helpers
-//Format raw entry for ardustat
 function toArd(command,value)
 {
+	last_comm = ardupadder(command,value)
 	serialPort.write(ardupadder(command,value));	
 }
+
+serialPort.on("data", function (data) {
+	if (data.search("GO")>-1)
+	{
+		foo = data_parse(data);
+		d = new Date().getTime()	
+		foo['time'] = d
+		
+		if (calibrate)
+		{
+			calibration_array.push(foo)
+		}
+		
+		if (logger=="started")
+		{
+			exec("echo '"+JSON.stringify(foo)+"' >> data/"+datafile);
+			
+		}
+		foo['logger'] = logger	 	
+		foo['datafile'] = datafile
+		io.sockets.emit('new_data',{'ardudata':foo} )
+		app.set('ardudata',foo)
+		
+
+	}
+		
+	
+});
+
+
 function ardupadder(command,number)
 {
 	number = parseInt(number)
