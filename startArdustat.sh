93221be4a02d46c31f174513d0de578960ac3b03
Ameer Ellaboudy <aellaboudy@imprintenergy.com>
2012-02-09 18:27:32 -0800

    Few fixes for the convenience file

--- startArdustat.sh
+++ startArdustat.sh
@@ -12,15 +12,14 @@ PORT=/dev/ttyACM$port_num
 while true; do
     read -p "Start viewer?" yn
     case $yn in
-        [Yy]* ) gnome-terminal -e "mongod"; cd NodeJS/Ardustat_Viewer; gnome-terminal -e "node view_server.js"; break;;
+        [Yy]* ) gnome-terminal -e "mongod"; cd NodeJS/Ardustat_Viewer; gnome-terminal -e "node view_server.js"; cd ../../;  break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-cd ../Ardustat_Control
+cd NodeJS/Ardustat_Control
 
 gnome-terminal -e "node expresserver.js $PORT $((8888+$port_num))"
 
-google-chrome "http://localhost:$((8888+$port_num))" "http://localhost:8000"
-
+google-chrome "http://localhost:$((8888+$port_num))/cycler" "http://localhost:$((8888+$port_num))/debug" "http://localhost:8000"
