52112005e818dfef4134326edc48fb3961aca915
Ameer Ellaboudy <aellaboudy@imprintenergy.com>
2012-02-24 15:30:25 -0800

    New start script with more automation

--- startArdustat.sh
+++ startArdustat.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-echo -e "Pick one serial port number:"
+#echo -e "Pick one serial port number:"
 
-ls /dev/ttyACM*
+#ls /dev/ttyACM*
 
-read port_num
+NUM_PORTS=$(ls -l /dev/ttyACM* | wc -l)
 
-PORT=/dev/ttyACM$port_num
+#read port_num
+
+google-chrome "http://localhost:8000"&
+mongod&
+cd NodeJS/Ardustat_Viewer
+node view_server.js&
 
 
-while true; do
-    read -p "Start viewer?" yn
-    case $yn in
-        [Yy]* ) gnome-terminal -e "mongod"; cd NodeJS/Ardustat_Viewer; gnome-terminal -e "node view_server.js"; cd ../../;  break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
+cd ../Ardustat_Control
+
+for ((  i = 0 ;  i < $NUM_PORTS;  i++  ))
+do
+  PORT=/dev/ttyACM$i
+  node expresserver.js $PORT $((8888+$i))&
+  google-chrome "http://localhost:$((8888+$i))/cycler"&
 done
 
-cd NodeJS/Ardustat_Control
 
-gnome-terminal -e "node expresserver.js $PORT $((8888+$port_num))"
 
-google-chrome "http://localhost:$((8888+$port_num))/cycler" "http://localhost:$((8888+$port_num))/debug" "http://localhost:8000"
+
+#while true; do
+#    read -p "Start viewer?" yn
+#    case $yn in
+#        [Yy]* ) gnome-terminal -e "mongod"; cd NodeJS/Ardustat_Viewer; gnome-terminal -e "node view_server.js"; cd ../../;  break;;
+#        [Nn]* ) break;;
+#        * ) echo "Please answer yes or no.";;
+#    esac
+#done
