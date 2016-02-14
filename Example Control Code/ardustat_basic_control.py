406e27097f437dc922dc83c3b36c0c08c215d1ef
Dan Steingart <dan.steingart@gmail.com>
2016-02-14 08:45:23 -0500

    better test file

--- Example Control Code/ardustat_basic_control.py
+++ Example Control Code/ardustat_basic_control.py
@@ -12,8 +12,6 @@ def write(ss):
     uo(url+"/write/"+ss).read()
     #sleep(.1)
 
-
-
 #lin_pot = [99.0 for x in range(256)] #
 
 #If we have a resistance table great, otherwise, make a linear map from 100 to 10000 kohm
@@ -35,9 +33,7 @@ def parse(reading):
     out['RR'] = res[int(out['RES'])]
     return out
     
-
 ref = parse(read())['ref']
-
 
 def setPot(val):
     #set potential in volts
@@ -72,15 +68,33 @@ def setCur(current,fix_res=None):
     outv = int(round(outv/ref))
     if outv < 0: outv = abs(outv)+2000
     gset = "g"+str(outv).rjust(4,"0")
+    delay(.1)
     write(gset)
-
-
 
 def setOCV():
     write("-0000")
 
+t0 = time()
+fn = "datafile_%i.csv" % t0
+header = 'time_s,potential_V,current_A\n'
+open(fn,'w').write(header)
+
+def writereading():
+    p = parse(read())
+    current = (out['ADC']-out['DAC'])/out['RR']
+    potential = out['ADC']-out['REF']
+    ts = time.time()-t0
+    out = "%f,%f,%f\n"
+    open(fn,'a').write(out)
+
+
 #some basic control code
 write("d0512")  #set the ardustat to work at +/- 2.5 V
-setPot(.1)
+setCur(.0001) #set to 100uA
+
+#take readings for 100 seconds
+while time.time() < (t0+100):
+    sleep(1)
+    writereading()    
 setOCV()
 
